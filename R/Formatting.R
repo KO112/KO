2f52fe9e7acb62185670a78647b35d9fd033e092
KO112-LT <kevinobrien112@outlook.com>
2019-02-17 01:57:53 -0500

    Rename formatting file

--- R/Formatting.R
+++ R/Formatting.R
@@ -1,5 +1,6 @@
-# Include external operators
-#' @include utils-pipe.R
+# Include other functions in package
+#' @include Pipes.R
+#' @include Error_Handling.R
 NULL
 
 # Avoid "undefined variable" notes in package checking
@@ -8,12 +9,16 @@ globalVariables(c("."))
 
 #' Format a numerical vector as percents
 #'
-#' @param x Object to format as a percent
+#' Format a numerical vector as a character vector holding the values formatted as percents.
+#' Will throw an error for non-numeric vetcors, and  print a message if NA values are found.
+#'
+#' @param vec Vector to format as a percent
 #' @param accuracy The number to round to the nearest multiple of
 #'   (e.g 3.14159 will round to 314.16% by default).
 #'   The absolutely value of the value passed will be used.
 #'   The rounding will be done after conversion to a percent.
 #' @param vecNames Vector used to name the output
+#' @param silent If TRUE, will not print out a message if NA values are found
 #'
 #' @return Character vector of items formatted as percent.
 #' @export
@@ -22,9 +27,13 @@ globalVariables(c("."))
 #' format_percent(0.12345)
 #' format_percent(pi)
 #' format_percent(0.1)
+#' format_percent(0.12345, accuracy = 0.1)
+#' format_percent(c(0.12345, 0.54321), vecNames = c("a", "b"))
 #'
-format_percent <- function(x, accuracy = 0.01, vecNames = names(x)) {
-  scales::percent(x = x, accuracy = abs(accuracy)) %>%
+format_percent <- function(vec, accuracy = 0.01, vecNames = names(vec), silent = FALSE) {
+  stop_if(!is.numeric(vec), "Non-numeric vector passed.")
+  if (any(is.na(vec)) & !silent) message("NA values were found, and will be printed as \"NA%\"")
+  scales::percent(x = vec, accuracy = abs(accuracy)) %>%
     stats::setNames(vecNames) %>%
     return()
 }
