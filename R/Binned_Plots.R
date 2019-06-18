7db8a417733af8114cfc39c1a5da2ad7dbf3f8ad
KO112 <kevin.obrien@rsagroup.ca>
2019-06-18 19:07:57 -0400

    BP: show weights changes, change axes

--- R/Binned_Plots.R
+++ R/Binned_Plots.R
@@ -46,7 +46,7 @@ binned_one_way_data <- function(x, yData, weight = rep(1, nrow(yData)), type = "
 }
 
 
-binned_one_way_plot <- function(x, yData, weight = rep(1, nrow(yData)), type = "equal", bins = 10, fontSize = 10,
+binned_one_way_plot <- function(x, yData, weight = rep(1, nrow(yData)), type = "quantile", bins = 10, fontSize = 10,
                                 showWeights = TRUE, overlap = TRUE, # plotly = FALSE,
                                 xlab = "Bins", ylab = "Response", wlab = "Weight", title = "One-Way Plot") {
 
@@ -60,6 +60,7 @@ binned_one_way_plot <- function(x, yData, weight = rep(1, nrow(yData)), type = "
     ggplot2::geom_bar(color = "black", stat = "identity", width = 1) +
     ggplot2::labs(x = xlab, y = wlab) +
     ggplot2::scale_x_continuous(limits = c(0.5, max(binnedData$index) + 0.5), breaks = binnedData$index, labels = binnedData$binnedCol__) +
+    ggplot2::scale_y_continuous(labels = scales::comma) +
     ggplot2::theme(
       text = ggplot2::element_text(size = fontSize)
       , axis.text.x = ggplot2::element_text(angle = 22.5, hjust = 1)
@@ -71,25 +72,36 @@ binned_one_way_plot <- function(x, yData, weight = rep(1, nrow(yData)), type = "
     ggplot2::aes(x = index, y = value, color = variable) +
     ggplot2::geom_line(size = 1) +
     ggplot2::geom_point(size = 2) +
-    ggplot2::labs(x = NULL, y = ylab, title = title) +
-    ggplot2::scale_x_continuous(limits = c(0.5, max(binnedData$index) + 0.5)) +
+    ggplot2::labs(x = xlab, y = ylab, title = title) +
+    ggplot2::scale_x_continuous(limits = c(0.5, max(binnedData$index) + 0.5), breaks = binnedData$index, labels = binnedData$binnedCol__) +
+    ggplot2::scale_y_continuous(labels = scales::comma) +
     ggplot2::theme(
       legend.position = "top"
       , text = ggplot2::element_text(size = fontSize)
       , plot.title = ggplot2::element_text(hjust = 0.5)
-      , axis.text.x = ggplot2::element_blank()
-      , axis.ticks.x = ggplot2::element_blank()
+      , axis.text.x = ggplot2::element_text(angle = 22.5, hjust = 1)
     )
+  browser()
+  # If the weights should be shown, merge and align them, else return just the plotted data
+  if (showWeights) {
 
-  # Align the plots better (see https://github.com/tidyverse/ggplot2/wiki/align-two-plots-on-a-page)
-  dataPlot <- ggplot2::ggplot_gtable(ggplot2::ggplot_build(dataPlot))
-  weightPlot <- ggplot2::ggplot_gtable(ggplot2::ggplot_build(weightPlot))
-  combinedPlot <- rbind(dataPlot, weightPlot, size = "first")
-  combinedPlot$widths <- grid::unit.pmax(dataPlot$widths, weightPlot$widths)
+    # Remove x-axis label, text and ticks
+    dataPlot <- dataPlot +
+      ggplot2::labs(x = NULL) +
+      ggplot2::theme(axis.text.x = ggplot2::element_blank(), axis.ticks.x = ggplot2::element_blank())
 
-  # Return the plots (merging into one if desired)
-  if (showWeights) return(gridExtra::arrangeGrob(dataPlot, weightPlot))
-  if (showWeights) return(combinedPlot)
-  return(dataPlot)
+    # Align the plots nicely (see https://github.com/tidyverse/ggplot2/wiki/align-two-plots-on-a-page)
+    dataPlot <- ggplot2::ggplot_gtable(ggplot2::ggplot_build(dataPlot))
+    weightPlot <- ggplot2::ggplot_gtable(ggplot2::ggplot_build(weightPlot))
+    combinedPlot <- rbind(dataPlot, weightPlot, size = "first")
+    combinedPlot$widths <- grid::unit.pmax(dataPlot$widths, weightPlot$widths)
+    return(combinedPlot)
+
+  } else {
+
+    return(dataPlot)
+    # return(plotly::ggplotly(dataPlot))
+
+  }
 
 }
