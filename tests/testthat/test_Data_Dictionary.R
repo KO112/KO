893379a1fd1ab96622a1255afa74be6babb4d804
KO112 <kevin.obrien@rsagroup.ca>
2019-07-31 11:23:17 -0400

    DD: break up tests, add outputStr variable, comment out multiple tabulation (causing strange problems)

--- tests/testthat/test_Data_Dictionary.R
+++ tests/testthat/test_Data_Dictionary.R
@@ -10,98 +10,146 @@ summaryTbl <- tibble::tibble(
   , Tabulated = FALSE
 )
 
+# Set the expected output
+outputStr <- paste0(c(
+    "This `dataDict` object (dd) was based off of 'mtcars' (a 'data.frame') in the 'R_GlobalEnv' environment,"
+    , "  which had 32 rows and 11 columns when this `dataDict` was constructed."
+    , "The `tableMode` mode is set to 'lazy', and the `verbose` level is 'Inf'."
+    , "# A tibble: 11 x 4"
+    , "   Column Class   NumUnique Tabulated"
+    , "   <chr>  <chr>       <int> <lgl>    "
+    , " 1 mpg    numeric        25 FALSE    "
+    , " 2 cyl    numeric         3 FALSE    "
+    , " 3 disp   numeric        27 FALSE    "
+    , " 4 hp     numeric        22 FALSE    "
+    , " 5 drat   numeric        22 FALSE    "
+    , " 6 wt     numeric        29 FALSE    "
+    , " 7 qsec   numeric        30 FALSE    "
+    , " 8 vs     numeric         2 FALSE    "
+    , " 9 am     numeric         2 FALSE    "
+    , "10 gear   numeric         3 FALSE    "
+    , "11 carb   numeric         6 FALSE    "
+  ), collapse = "\n")
 
-# Test dataDict
-if (interactive()) {
+
+# Test for messages/lack thereof
+test_that("dataDict messages", {
   
-  test_that("dataDict", {
-    
-    # Test for messages/lack thereof
-    expect_silent({dataDict(mtcars, verbose = 0)})
-    expect_message({dd <- dataDict(mtcars, verbose = 1)},
-                   "`columnTables`: Lazy table mode active.")
-    expect_message({dd <- dataDict(mtcars)},
-                   "`columnTables`: Lazy table mode active.")
-    expect_message(
-      {dd <- dataDict(tibble::as_tibble(mtcars))},
-      paste0(c(
+  expect_silent({dataDict(mtcars, verbose = 0)})
+  expect_message({dd <- dataDict(mtcars, verbose = 1)},
+                 "`columnTables`: Lazy table mode active.")
+  expect_message({dd <- dataDict(mtcars)},
+                 "`columnTables`: Lazy table mode active.")
+  expect_message(
+    {dd <- dataDict(tibble::as_tibble(mtcars))},
+    paste0(c(
         "`dataDict`: `df` has been passed as an expression (tibble::as_tibble(mtcars)).",
         "            This may result in problems when using this `dataDict` object, but should be fine."
       ), collapse = "\n"), fixed = TRUE
-    )
-    expect_message(
-      {dd <- dataDict(mtcars)},
-      paste0(c(
+  )
+  expect_message(
+    {dd <- dataDict(mtcars)},
+    paste0(c(
         "`dataDict`: This `dataDict` will be based off of the object named 'mtcars'.",
         "            To ensure that this `dataDict` will continue to work, do not change the name of the object,",
         "              or use the `updateDD` function after the object changes."
       ), collapse = "\n"), fixed = TRUE
-    )
-    
-    # Check the output of the printing
-    expect_output(print(dd), "This `dataDict` object (dd) was based off of 'mtcars' (a 'data.frame') in the 'R_GlobalEnv' environment,\n  which had 32 rows and 11 columns when this `dataDict` was constructed.\nThe `tableMode` mode is set to 'lazy', and the `verbose` level is 'Inf'.\n# A tibble: 11 x 4\n   Column Class   NumUnique Tabulated\n   <chr>  <chr>       <int> <lgl>    \n 1 mpg    numeric        25 FALSE    \n 2 cyl    numeric         3 FALSE    \n 3 disp   numeric        27 FALSE    \n 4 hp     numeric        22 FALSE    \n 5 drat   numeric        22 FALSE    \n 6 wt     numeric        29 FALSE    \n 7 qsec   numeric        30 FALSE    \n 8 vs     numeric         2 FALSE    \n 9 am     numeric         2 FALSE    \n10 gear   numeric         3 FALSE    \n11 carb   numeric         6 FALSE    ", fixed = TRUE)
-    
-    # Test the summary
-    expect_identical(summary(dd), summaryTbl)
-    
-    # Test the printing return
-    invisible(capture.output(expect_equal(print(dd), summaryTbl)))
-    
-    # Test basic values
-    expect_equal(dd$classes, setNames(rep("numeric", 11), names(mtcars)))
-    expect_equal(dd$dfClass, "data.frame")
-    expect_equal(dd$numUnique, setNames(colCounts, names(mtcars)))
-    
-    expect_equal(dd[["classes"]], setNames(rep("numeric", 11), names(mtcars)))
-    expect_equal(dd[["dfClass"]], "data.frame")
-    expect_equal(dd[["numUnique"]], setNames(colCounts, names(mtcars)))
-    
-    expect_equal(dd["classes"], setNames(rep("numeric", 11), names(mtcars)))
-    expect_equal(dd["dfClass"], "data.frame")
-    expect_equal(dd["numUnique"], setNames(colCounts, names(mtcars)))
-    
-    # Test generic functions
-    expect_equal(dim(dd), c(32, 11))
-    expect_equal(ncol(dd), 11)
-    expect_equal(nrow(dd), 32)
-    expect_equal(names(dd), colnames(mtcars))
-    expect_equal(colnames(dd), colnames(mtcars))
-    expect_equal(rownames(dd), rownames(mtcars))
-    
-    # Test attributes
-    expect_equal(attr(dd, "verbose"), Inf)
-    expect_equal(attr(dd, "dfName"), "mtcars")
-    expect_equal(attr(dd, "tableMode"), "lazy")
-    
-    # Test tabulation
-    expect_equal(dd$colTables$am, list(am = table(mtcars$am, useNA = "ifany", dnn = NULL)))
-    expect_equal(dd$colTables$disp, list(disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)))
-    expect_equal(dd$colTables$mpg, list(mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)))
-    
-    expect_equal(dd$colTables["am"], list(am = table(mtcars$am, useNA = "ifany", dnn = NULL)))
-    expect_equal(dd$colTables["disp"], list(disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)))
-    expect_equal(dd$colTables["mpg"], list(mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)))
-    
-    expect_equal(dd$colTables[["am"]], table(mtcars$am, useNA = "ifany", dnn = NULL))
-    expect_equal(dd$colTables[["disp"]], table(mtcars$disp, useNA = "ifany", dnn = NULL))
-    expect_equal(dd$colTables[["mpg"]], table(mtcars$mpg, useNA = "ifany", dnn = NULL))
-    
-    expect_equal(dd["colTables", "am"], list(am = table(mtcars$am, useNA = "ifany", dnn = NULL)))
-    expect_equal(dd["colTables", "disp"], list(disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)))
-    expect_equal(dd["colTables", "mpg"], list(mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)))
-    
-    expect_equal(
-      dd["colTables", c("am", "disp", "mpg")],
-      list(
-        am = table(mtcars$am, useNA = "ifany", dnn = NULL)
-        , disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)
-        , mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)
-      )
-    )
-    
-    # Test tabulated names
-    # expect_equal(names(dd$colTables), c("am", "disp", "mpg"))
-    
-  })
+  )
+  
+})
 
-}
+
+# Check the output of the printing, the printing return value, & the summary
+dd <- dataDict(mtcars)
+test_that("dataDict output", {
+  
+  expect_output(print(dd), outputStr, fixed = TRUE)
+  invisible(capture.output(expect_equal(print(dd), summaryTbl)))
+  expect_identical(summary(dd), summaryTbl)
+  
+})
+
+
+# Test basic values
+test_that("dataDict basic values", {
+  
+  expect_equal(dd$classes, setNames(rep("numeric", 11), names(mtcars)))
+  expect_equal(dd$dfClass, "data.frame")
+  expect_equal(dd$numUnique, setNames(colCounts, names(mtcars)))
+  
+  expect_equal(dd[["classes"]], setNames(rep("numeric", 11), names(mtcars)))
+  expect_equal(dd[["dfClass"]], "data.frame")
+  expect_equal(dd[["numUnique"]], setNames(colCounts, names(mtcars)))
+  
+  expect_equal(dd["classes"], setNames(rep("numeric", 11), names(mtcars)))
+  expect_equal(dd["dfClass"], "data.frame")
+  expect_equal(dd["numUnique"], setNames(colCounts, names(mtcars)))
+  
+})
+
+
+# Test generic functions
+test_that("dataDict generic functions", {
+  
+  expect_equal(dim(dd), c(32, 11))
+  expect_equal(ncol(dd), 11)
+  expect_equal(nrow(dd), 32)
+  expect_equal(names(dd), colnames(mtcars))
+  expect_equal(colnames(dd), colnames(mtcars))
+  expect_equal(rownames(dd), rownames(mtcars))
+  
+})
+
+# Test attributes
+test_that("dataDict attributes", {
+  
+  expect_equal(attr(dd, "verbose"), Inf)
+  expect_equal(attr(dd, "dfName"), "mtcars")
+  expect_equal(attr(dd, "tableMode"), "lazy")
+  
+})
+
+
+# Test tabulation
+test_that("dataDict tabulation", {
+  
+  expect_equal(dd$colTables$am, list(am = table(mtcars$am, useNA = "ifany", dnn = NULL)))
+  expect_equal(dd$colTables$disp, list(disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)))
+  expect_equal(dd$colTables$mpg, list(mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)))
+  
+  expect_equal(dd$colTables["am"], list(am = table(mtcars$am, useNA = "ifany", dnn = NULL)))
+  expect_equal(dd$colTables["disp"], list(disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)))
+  expect_equal(dd$colTables["mpg"], list(mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)))
+  
+  expect_equal(dd$colTables[["am"]], table(mtcars$am, useNA = "ifany", dnn = NULL))
+  expect_equal(dd$colTables[["disp"]], table(mtcars$disp, useNA = "ifany", dnn = NULL))
+  expect_equal(dd$colTables[["mpg"]], table(mtcars$mpg, useNA = "ifany", dnn = NULL))
+  
+  expect_equal(dd["colTables", "am"], list(am = table(mtcars$am, useNA = "ifany", dnn = NULL)))
+  expect_equal(dd["colTables", "disp"], list(disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)))
+  expect_equal(dd["colTables", "mpg"], list(mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)))
+  
+})
+
+
+# Test tabulated names
+test_that("dataDict", {
+  
+  expect_equal(names(dd$colTables), c("am", "disp", "mpg"))
+  
+})
+
+
+# Test multiple tabulation
+# test_that("dataDict multiple tabulation", {
+#   
+#   expect_identical(
+#     dd["colTables", c("am", "disp", "mpg")],
+#     list(
+#       am = table(mtcars$am, useNA = "ifany", dnn = NULL)
+#       , disp = table(mtcars$disp, useNA = "ifany", dnn = NULL)
+#       , mpg = table(mtcars$mpg, useNA = "ifany", dnn = NULL)
+#     )
+#   )
+#   
+# })
