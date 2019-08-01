cb9496ebb1d5e943870d3cb9a516d36235e83ed0
KO112 <kevin.obrien@rsagroup.ca>
2019-08-01 15:26:13 -0400

    Coloured_Output/info (CO/I): fix params docs, add printOut param

--- R/Colored_Output.R
+++ R/Colored_Output.R
@@ -3,8 +3,10 @@
 #' Used to print out some information during the execution of a function.
 #' This function prints out the text in green (#00FF00) using the \code{crayon} package.
 #' 
-#' @param str The text to print out (character scalar/vector).
+#' @param ... Text to print out (can also be named arguments to \code{message}/\code{cat},
+#'   e.g. \code{sep}, which defaults to " ").
 #' @param useMsg Whether to use \code{message} to print the text (uses \code{cat} otherwise).
+#' @param printOut Whether to print the value out (might just want the colored string raw).
 #' 
 #' @return The colored string, invisibly.
 #' @export
@@ -12,8 +14,10 @@
 #' @examples
 #' info("hello world")
 #' 
-info <- function(..., useMsg = TRUE) {
+info <- function(..., useMsg = TRUE, printOut = TRUE) {
   colStr <- crayon::make_style("#00FF00")(...)
-  if (useMsg) message(colStr) else cat(colStr)
+  if (printOut) {
+    if (useMsg) message(colStr) else cat(colStr, "\n")
+  }
   return(invisible(colStr))
 }
