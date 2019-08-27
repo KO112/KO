65ca5bb1f2f7dcba9d92481fd5aae98d21c5eb06
KO112 <kevin.obrien@rsagroup.ca>
2019-08-27 10:54:21 -0400

    AC: clean code, remove unneeded code, rename vars, remove buttons

--- R/Auto_Complete.R
+++ R/Auto_Complete.R
@@ -16,7 +16,6 @@ auto_complete_var <- function() {
   contents <- context$contents
   selection <- context$selection %>% rstudioapi::primary_selection()
   start <- selection$range$start
-  # end <- selection$range$end
   
   # Set the variables search text, & the output range
   if (selection$text != "") {
@@ -46,14 +45,8 @@ auto_complete_var <- function() {
          length(varMatches), " elements were selected.")
   }
   
-  # ifelse(length(.) <= 1, ., paste0("${", seq_along(.), ":", ., "}", collapse = ", ")) %>%
-  #   rstudioapi::insertText(outputRange, ., context$id)
-  
-  # rstudioapi::setSelectionRanges(list(c(1, 1, 1, 5), c(1, 6, 1, 10)), context$id)
-  
 }
 # auto_complete_var()
-
 
 
 
@@ -62,34 +55,33 @@ tkDropDown <- function(opts, title = "Select a Word") {
   
   # Check that we have version 8.5 or later, & set the tkbutton object
   have_ttk <- as.character(tcl("info", "tclversion")) >= "8.5"
-  if (!have_ttk) ttkbutton <- tkbutton
+  # if (!have_ttk) ttkbutton <- tkbutton
   
   # Set the tcl object, adding the desired options
   lvar <- tclVar()
   tclObj(lvar) <- opts
   
-  # Set the service mode, & cretae the top-level widget
-  oldmode <- tclServiceMode(FALSE)
+  # Hide the temporary widget, storing the original modal setting, & create the top-level widget
+  oldMode <- tclServiceMode(FALSE)
   dlg <- tktoplevel()
   
   # Set the title, remove the icon, get the set, & focus the widget
   tkwm.title(dlg, title)
-  tkwm.deiconify(dlg)
-  tkgrab.set(dlg)
-  tkfocus(dlg)
+  # tkwm.deiconify(dlg)
+  # tkgrab.set(dlg)
+  # tkfocus(dlg)
   
-  # 
-  if (!is.null(title) && nzchar(title)) {
+  # Set the title label
+  if (title != "") {
     lab <- if (have_ttk) ttklabel(dlg, text = title, foreground = "blue")
-    else tklabel(dlg, text = title, fg = "blue")
+           else tklabel(dlg, text = title, fg = "blue")
     tkpack(lab, side = "top")
   }
   
   # Runs when the form is submitted
   onOK <- function() {
-    res <- 1L + as.integer(tkcurselection(box))
-    # res2 <<- tknearest(box)
-    ans.select_list <<- opts[res]
+    res <- 1L + as.integer(tkcurselection(listBox))
+    selectedVar <<- opts[res]
     tkgrab.release(dlg)
     tkdestroy(dlg)
   }
@@ -101,69 +93,68 @@ tkDropDown <- function(opts, title = "Select a Word") {
   }
   
   # 
-  buttons <- tkframe(dlg)
-  tkpack(buttons, side = "bottom")
-  OK <- ttkbutton(buttons, text = gettext("OK"), width = 6, command = onOK)
-  Cancel <- ttkbutton(buttons, text = gettext("Cancel"), command = onCancel)
-  tkpack(OK, Cancel, side = "left", fill = "x", padx = "2m")
+  # buttons <- tkframe(dlg)
+  # tkpack(buttons, side = "bottom")
+  # OK <- ttkbutton(buttons, text = gettext("OK"), width = 6, command = onOK)
+  # Cancel <- ttkbutton(buttons, text = gettext("Cancel"), command = onCancel)
+  # tkpack(OK, Cancel, side = "left", fill = "x", padx = "2m")
   
   # 
   scht <- as.numeric(tclvalue(tkwinfo("screenheight", dlg))) - 200L
   ht <- min(length(opts), scht %/% 20)
   
   # 
-  s_mode <- if (multiple) "multiple" else "single"
-  box <- tklistbox(dlg, height = ht, listvariable = lvar, bg = "white", setgrid = 1, selectmode = s_mode)
-  tmp <- tcl("font", "metrics", tkcget(box, font = NULL))
+  selectionMode <- "browse" # "single"
+  listBox <- tklistbox(dlg, height = ht, listvariable = lvar, bg = "white", setgrid = 1, selectmode = selectionMode)
+  tmp <- tcl("font", "metrics", tkcget(listBox, font = NULL))
   tmp <- as.numeric(sub(".*linespace ([0-9]+) .*", "\\1", tclvalue(tmp))) + 3
   ht <- min(length(opts), scht %/% tmp)
   
-  # 
-  tkdestroy(box)
+  # Destroy the temporary list box
+  tkdestroy(listBox)
   
   # 
   if (ht < length(opts)) {
     
     # 
-    scr <- if (have_ttk) ttkscrollbar(dlg, command = function(...) tkyview(box, ...))
-      else tkscrollbar(dlg, repeatinterval = 5, command = function(...) tkyview(box, ...))
+    scr <- if (have_ttk) ttkscrollbar(dlg, command = function(...) tkyview(listBox, ...))
+      else tkscrollbar(dlg, repeatinterval = 5, command = function(...) tkyview(listBox, ...))
     
     # 
-    box <- tklistbox(dlg, height = ht, width = 0, listvariable = lvar, bg = "white", setgrid = 1,
-                     selectmode = s_mode, yscrollcommand = function(...) tkset(scr, ...))
-    tkpack(box, side = "left", fill = "both", expand = TRUE)
+    listBox <- tklistbox(dlg, height = ht, width = 0, listvariable = lvar, bg = "white", selectmode = selectionMode,
+                         setgrid = 1, yscrollcommand = function(...) tkset(scr, ...))
+    tkpack(listBox, side = "left", fill = "both", expand = TRUE)
     tkpack(scr, side = "right", fill = "y")
     
   } else {
     
     # 
-    box <- tklistbox(dlg, height = ht, width = 0, listvariable = lvar, bg = "white", selectmode = s_mode)
-    tkpack(box, side = "left", fill = "both")
+    listBox <- tklistbox(dlg, height = ht, width = 0, listvariable = lvar, bg = "white", selectmode = selectionMode)
+    tkpack(listBox, side = "left", fill = "both")
     
   }
   
-  # 
-  tkselection.set(box, 1L)
-  tkyview(box, 1L)
-  ans.select_list <- character()
+  # Select the first element
+  tkselection.set(listBox, 0L)
+  selectedVar <- character(0)
   
-  # 
-  tkbind(dlg, "<Destroy>", onCancel)
-  tkbind(box, "<Double-ButtonPress-1>", onOK)
+  # Set key bindings
+  tkbind(listBox, "<Double-ButtonPress-1>", onOK)
   tkbind(dlg, "<Return>", onOK)
-  # tkbind(dlg, "<Esc>", onCancel)
+  tkbind(dlg, "<space>", onOK)
+  tkbind(dlg, "<Destroy>", onCancel)
+  tkbind(dlg, "<Escape>", onCancel)
+  # tkbind(dlg, "<Up>", onUp)
+  # tkbind(dlg, "<Down>", onDown)
   
-  # 
-  tkfocus(box)
-  tclServiceMode(oldmode)
+  # Activate the list box, show it modally, activate it, & wait for the user to take an action
+  tkfocus(listBox)
+  tclServiceMode(TRUE) # oldMode)
   # system(paste0("WScript \"C:/Users/tae8766/Documents/GitHub/General/AutoHotKey/Scripts/Activate Window.vbs\" \"", title, "\""))
-  # browser()
   system(paste0("\"C:/Users/tae8766/Documents/GitHub/General/AutoHotKey/Scripts/Activate Window.exe\" \"", title, "\""))
   tkwait.window(dlg)
   
-  # 
-  Sys.sleep(0.1)
-  if (!multiple && !length(ans.select_list)) ans.select_list <- ""
-  return(ans.select_list)
+  # Return the selection
+  return(selectedVar)
   
 }
