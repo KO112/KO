d375ec689eded4afe99b10ff6a8c4d02104c77f9
KO112 <kevin.obrien@rsagroup.ca>
2020-06-01 11:37:28 -0400

    CAI: T -> TRUE, remove redundant "()" from regex matching for run_fn

--- R/Console_Add-Ins.R
+++ R/Console_Add-Ins.R
@@ -35,7 +35,7 @@ run_fn <- function() {
     editor <- rstudioapi::getSourceEditorContext()
     selection <- rstudioapi::primary_selection(editor$selection)
     code <- utils::head(editor$contents, selection$range$start[["row"]])
-    lastFn <- grep("^\\w+[ ]*<-[ ]*function()", code, ignore.case = T, value = T) %>%
+    lastFn <- grep("^\\w+[ ]*<-[ ]*function", code, ignore.case = TRUE, value = TRUE) %>%
       {ifelse(length(.) > 0, utils::tail(., 1), "")}
     execute <- rstudioapi::getConsoleEditorContext()$contents != ""
     
